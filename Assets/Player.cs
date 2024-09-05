b3ccfcac514aa00018e158e86a3971910b8e595f
hahawoqing <hsd169114@163.com>
2024-09-06 04:21:13 +0800

    第一次递交，人物动作初步完成

---
+++ Assets/Player.cs
@@ -0,0 +1,155 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    [Header("Dash info")]
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashDuration;
+    private float dashTime;
+    [SerializeField] private float dashCooldown;
+    private float dashCooldownTimer;
+    [Header("Attack info")]
+    private float comboTimeWindow;
+    [SerializeField] private float comboTime;
+    private bool isAttacking;
+    private int comboCounter;
+    private float xInput;
+    private int facingDir = 1;
+    private bool facingRight = true;
+    [Header("Collision info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGroud;
+    private bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        CheckInput();
+        CollisionChecks();
+
+        dashTime -= Time.deltaTime;
+        dashCooldownTimer -= Time.deltaTime;
+        comboTimeWindow -= Time.deltaTime;
+
+        FlipController();
+        AnimatorControllers();
+    }
+
+    public void AttackOver()
+    {
+        isAttacking = false;
+        comboCounter++;
+        comboCounter %= 3;
+    }
+    private void CollisionChecks()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGroud);
+    }
+
+    private void CheckInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            DashAbility();
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            StartAttackEvent();
+        }
+    }
+
+    private void StartAttackEvent()
+    {
+        if (dashTime > 0)
+        {
+            return;
+        }
+        if (comboTimeWindow < 0)
+        {
+            comboCounter = 0;
+        }
+        isAttacking = true;
+        comboTimeWindow = comboTime;
+    }
+
+    private void DashAbility()
+    {
+        if (dashCooldownTimer < 0)
+        {
+            dashCooldownTimer = dashCooldown;
+            dashTime = dashDuration;
+        }
+    }
+
+    private void Movement()
+    {
+        if (dashTime > 0)
+        {
+            rb.velocity = new Vector2(facingDir * dashSpeed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+        }
+    }
+
+    private void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    private void AnimatorControllers()
+    {
+        bool isMoving = rb.velocity.x != 0;
+
+        anim.SetFloat("yVelocity", rb.velocity.y);
+        anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isDashing", dashTime > 0);
+        anim.SetBool("isAttacking", isAttacking);
+        anim.SetInteger("comboCounter", comboCounter);
+    }
+
+    private void Flip()
+    {
+        facingDir *= -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+    private void FlipController()
+    {
+        if (rb.velocity.x > 0 && facingRight != true)
+        {
+            Flip();
+        }
+        else if (rb.velocity.x < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y-groundCheckDistance));
+    }
+}
